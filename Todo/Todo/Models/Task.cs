548bd58e86b68a17232dfa7fa89d10144493cea5
Daniel Berrocal <jdanielb1992@gmail.com>
2015-07-24 14:52:38 -0600

    First Commit

---
+++ Todo/Todo/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Todo.Models
+{
+    public class Task
+    {
+        public int TodoId { get; set; }
+        //[Required]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        //[Required]
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
